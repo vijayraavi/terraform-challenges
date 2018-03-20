075a3b3ac379b82a87bf3ca89ad727361c3880ce
Richard Cheney <richard.cheney@microsoft.com>
2018-03-20 13:14:21 +0000

    vmnic to use NSG

--- challenge1/vm.tf
+++ challenge1/vm.tf
@@ -2,6 +2,7 @@ resource "azurerm_network_interface" "vm" {
     name                = "${var.vmname}-nic"
     location            = "${var.region}"
     resource_group_name = "${azurerm_resource_group.rg.name}"
+    network_security_group_id = "${azurerm_network_security_group.linux.id}"
 
     ip_configuration {
         name                          = "myNicConfiguration"
