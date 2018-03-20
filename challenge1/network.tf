51bc9367b9a714fbd69bd5eac5b222653bbdabb1
Richard Cheney <richard.cheney@microsoft.com>
2018-03-20 12:24:04 +0000

    Removed tags from subnet - not supported

--- challenge1/network.tf
+++ challenge1/network.tf
@@ -15,11 +15,6 @@ resource "azurerm_subnet" "vm" {
     resource_group_name  = "${azurerm_resource_group.rg.name}"
     virtual_network_name = "${azurerm_virtual_network.vnet.name}"
     address_prefix       = "10.176.1.0/24"
-
-    tags = {
-        environment = "test"
-        description = "Technical Depth"
-    }
 }
 
 resource "azurerm_public_ip" "vm" {
