ae578caadc96d332432eb3c2abaf8694a4a9010f
Richard Cheney <richard.cheney@microsoft.com>
2018-03-20 12:22:43 +0000

    Resource group name

--- challenge1/diagnostics.tf
+++ challenge1/diagnostics.tf
@@ -1,7 +1,7 @@
 resource "random_id" "randomId" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
-        resource_group = "${azurerm_resource_group.vm.name}"
+        resource_group = "${azurerm_resource_group.rg.name}"
     }
 
     byte_length = 8
@@ -9,7 +9,7 @@ resource "random_id" "randomId" {
 
 resource "azurerm_storage_account" "diagstorage" {
     name = "diag${random_id.randomId.hex}"
-    resource_group_name = "${azurerm_resource_group.vm.name}"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
     location = "${var.region}"
     account_tier = "Standard"
     account_replication_type = "LRS"
--- challenge1/resourceGroups.tf
+++ challenge1/resourceGroups.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "vm" {
+resource "azurerm_resource_group" "rg" {
     name = "${var.rg}"
     location = "${var.region}"
 
