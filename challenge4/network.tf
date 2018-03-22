d5ebc5e9f26679a262a05fb9050809271c9f0219
richeney <richeney@microsoft.com>
2018-03-22 21:57:48 +0000

    Stupid boy

--- challenge4/network.tf
+++ challenge4/network.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "vnet" {
     address_space       = [ "10.76.0.0/16" ]
     location            = "${var.region}"
     resource_group_name = "${azurerm_resource_group.rg.name}"
-    tags                = "${var.tags}
+    tags                = "${var.tags}"
     }
 }
 
